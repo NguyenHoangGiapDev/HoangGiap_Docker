c305b5a1c9c6363c151733f848a8a3413db7b1d5
NguyenHoangGiap <nguyenhoanggiap04@gmail.com>
2026-03-16 14:50:57 +0700

    first commit

---
+++ src/main/java/com/example/nguyenhoanggiap/NguyenhoanggiapApplication.java
@@ -0,0 +1,13 @@
+package com.example.nguyenhoanggiap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenhoanggiapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenhoanggiapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nguyenhoanggiap/NguyenhoanggiapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nguyenhoanggiap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenhoanggiapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
